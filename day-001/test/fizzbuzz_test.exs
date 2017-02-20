515afa154d85d4ecbfd9000f80c877af8ba8f265
Cairo Noleto <caironoleto@gmail.com>
2017-02-20 20:38:40 -0300

    Adding more test

--- day-001/test/fizzbuzz_test.exs
+++ day-001/test/fizzbuzz_test.exs
@@ -9,4 +9,8 @@ defmodule FizzbuzzTest do
   test "returns 1, b/c 1 is not divisible by 3 or 5" do
     assert Fizzbuzz.replace(1) == 1
   end
+
+  test "returns 2, b/c 1 is not divisible by 3 or 5" do
+    assert Fizzbuzz.replace(2) == 2
+  end
 end
