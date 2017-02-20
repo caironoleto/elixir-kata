9d65bdb85694918f1ac3c4e8fa5740e55cff956a
Cairo Noleto <caironoleto@gmail.com>
2017-02-20 20:39:17 -0300

    Order tests

--- day-001/test/fizzbuzz_test.exs
+++ day-001/test/fizzbuzz_test.exs
@@ -2,15 +2,15 @@ defmodule FizzbuzzTest do
   use ExUnit.Case
   doctest Fizzbuzz
 
-  test "replaces 3 with fizz" do
-    assert Fizzbuzz.replace(3) == "fizz"
-  end
-
   test "returns 1, b/c 1 is not divisible by 3 or 5" do
     assert Fizzbuzz.replace(1) == 1
   end
 
   test "returns 2, b/c 1 is not divisible by 3 or 5" do
     assert Fizzbuzz.replace(2) == 2
+  end
+
+  test "replaces 3 with fizz" do
+    assert Fizzbuzz.replace(3) == "fizz"
   end
 end
