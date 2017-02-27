6c37a709efd80fac2a0b8c4ae55b125f4cda30fc
Cairo Noleto <caironoleto@gmail.com>
2017-02-26 21:49:37 -0300

    Adding first test

---
+++ day-002/lib/simple_statistics.ex
@@ -0,0 +1,5 @@
+defmodule SimpleStatistics do
+  def analyze(list) do
+    [{:minimum, 1}, {:maximum, 10}, {:size, 6}, {:mean, 5.5}]
+  end
+end
---
+++ day-002/test/simple_statistics_test.exs
@@ -0,0 +1,8 @@
+defmodule SimpleStatisticsTest do
+  use ExUnit.Case
+  doctest SimpleStatistics
+
+  test "process a list with two elements" do
+    assert SimpleStatistics.analyze([1, 10]) == [{:minimum, 1}, {:maximum, 10}, {:size, 6}, {:mean, 5.5}]
+  end
+end
