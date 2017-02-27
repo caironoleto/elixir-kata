68d104e6018ce1b1a2553686169b6e7be6008270
Cairo Noleto <caironoleto@gmail.com>
2017-02-26 22:02:33 -0300

    Adding Enum.min and Enum.max to return the minimum and maximum.

--- day-002/lib/simple_statistics.ex
+++ day-002/lib/simple_statistics.ex
@@ -1,6 +1,6 @@
 defmodule SimpleStatistics do
   def analyze(list) do
-    [{:minimum, 1}, {:maximum, 10}, {:size, length(list)}, {:mean, do_mean(list)}]
+    [{:minimum, Enum.min(list)}, {:maximum, Enum.max(list)}, {:size, length(list)}, {:mean, do_mean(list)}]
   end
 
   defp do_mean(list) do
--- day-002/test/simple_statistics_test.exs
+++ day-002/test/simple_statistics_test.exs
@@ -3,10 +3,14 @@ defmodule SimpleStatisticsTest do
   doctest SimpleStatistics
 
   test "process a list with two elements" do
-    assert SimpleStatistics.analyze([1, 10]) == [{:minimum, 1}, {:maximum, 10}, {:size, 2}, {:mean, 5.5}]
+    assert SimpleStatistics.analyze([1, 10]) == [minimum: 1, maximum: 10, size: 2, mean: 5.5]
   end
 
   test "process a list with three elements" do
-    assert SimpleStatistics.analyze([1, 4, 10]) == [{:minimum, 1}, {:maximum, 10}, {:size, 3}, {:mean, 5}]
+    assert SimpleStatistics.analyze([1, 4, 10]) == [minimum: 1, maximum: 10, size: 3, mean: 5]
+  end
+
+  test "process a list with four elements" do
+    assert SimpleStatistics.analyze([2, 4, 10, 20]) == [minimum: 2, maximum: 20, size: 4, mean: 9.0]
   end
 end
