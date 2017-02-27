90671ced59d3404e1682ebe5c837b946610242ed
Cairo Noleto <caironoleto@gmail.com>
2017-02-27 08:18:09 -0300

    Adding test to cover when rock wins

--- day-003/lib/jokenpo.ex
+++ day-003/lib/jokenpo.ex
@@ -1,6 +1,7 @@
 defmodule Jokenpo do
   def winner([:rock, other]) do
     cond do
+      other == :scissor -> :rock
       true -> other
     end
   end
--- day-003/test/jokenpo_test.exs
+++ day-003/test/jokenpo_test.exs
@@ -3,6 +3,10 @@ defmodule JokenpoTest do
   doctest Jokenpo
 
   describe "when the first move is rock" do
+    test "returns :rock when the other move is :scissor" do
+      assert Jokenpo.winner([:rock, :scissor]) == :rock
+    end
+
     test "returns :paper when the other move is :paper" do
       assert Jokenpo.winner([:rock, :paper]) == :paper
     end
