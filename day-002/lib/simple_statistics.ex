d62d2f4dda6ebafa10cfc18ffc607babab3a5936
Cairo Noleto <caironoleto@gmail.com>
2017-02-26 22:16:10 -0300

    Renaming mean to average and using a simple atom notation to write the tuples.

--- day-002/lib/simple_statistics.ex
+++ day-002/lib/simple_statistics.ex
@@ -1,9 +1,14 @@
 defmodule SimpleStatistics do
   def analyze(list) do
-    [{:minimum, Enum.min(list)}, {:maximum, Enum.max(list)}, {:size, length(list)}, {:mean, do_mean(list)}]
+    [
+      minimum: Enum.min(list),
+      maximum: Enum.max(list),
+      size: length(list),
+      average: do_average(list)
+    ]
   end
 
-  defp do_mean(list) do
+  defp do_average(list) do
     Enum.sum(list) / length(list)
   end
 end
--- day-002/test/simple_statistics_test.exs
+++ day-002/test/simple_statistics_test.exs
@@ -3,18 +3,18 @@ defmodule SimpleStatisticsTest do
   doctest SimpleStatistics
 
   test "process a list with two elements" do
-    assert SimpleStatistics.analyze([1, 10]) == [minimum: 1, maximum: 10, size: 2, mean: 5.5]
+    assert SimpleStatistics.analyze([1, 10]) == [minimum: 1, maximum: 10, size: 2, average: 5.5]
   end
 
   test "process a list with three elements" do
-    assert SimpleStatistics.analyze([1, 4, 10]) == [minimum: 1, maximum: 10, size: 3, mean: 5]
+    assert SimpleStatistics.analyze([1, 4, 10]) == [minimum: 1, maximum: 10, size: 3, average: 5]
   end
 
   test "process a list with four elements" do
-    assert SimpleStatistics.analyze([2, 4, 10, 20]) == [minimum: 2, maximum: 20, size: 4, mean: 9.0]
+    assert SimpleStatistics.analyze([2, 4, 10, 20]) == [minimum: 2, maximum: 20, size: 4, average: 9.0]
   end
 
   test "process a list with negative element" do
-    assert SimpleStatistics.analyze([-9, 2, 4, 10, 20, 100]) == [minimum: -9, maximum: 100, size: 6, mean: 21.166666666666668]
+    assert SimpleStatistics.analyze([-9, 2, 4, 10, 20, 100]) == [minimum: -9, maximum: 100, size: 6, average: 21.166666666666668]
   end
 end
