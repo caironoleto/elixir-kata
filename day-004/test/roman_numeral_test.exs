92b6827180a83efcc51d2839943e2d386a98160a
Cairo Noleto <caironoleto@gmail.com>
2017-02-28 17:45:35 -0300

    Adding tests to cover CC, CCC, MM, MMM

--- day-004/test/roman_numeral_test.exs
+++ day-004/test/roman_numeral_test.exs
@@ -12,6 +12,10 @@ defmodule RomanNumeralTest do
   test "converts XXX to 30", do: assert to_number("XXX") == 30
   test "converts L to 50", do: assert to_number("L") == 50
   test "converts C to 100", do: assert to_number("C") == 100
+  test "converts CC to 200", do: assert to_number("CC") == 200
+  test "converts CCC to 300", do: assert to_number("CCC") == 300
   test "converts D to 500", do: assert to_number("D") == 500
   test "converts M to 1000", do: assert to_number("M") == 1000
+  test "converts MM to 2000", do: assert to_number("MM") == 2000
+  test "converts MMM to 3000", do: assert to_number("MMM") == 3000
 end
