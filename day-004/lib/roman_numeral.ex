7d4956c1aa94ce4a288666bf550a75c9e0ce1770
Cairo Noleto <caironoleto@gmail.com>
2017-02-28 14:10:10 -0300

    Adding 10 test

--- day-004/lib/roman_numeral.ex
+++ day-004/lib/roman_numeral.ex
@@ -1,4 +1,5 @@
 defmodule RomanNumeral do
   def to_number(1), do: "I"
   def to_number(5), do: "V"
+  def to_number(10), do: "X"
 end
--- day-004/test/roman_numeral_test.exs
+++ day-004/test/roman_numeral_test.exs
@@ -4,4 +4,5 @@ defmodule RomanNumeralTest do
 
   test "converts 1 to I", do: assert to_number(1) == "I"
   test "converts 5 to V", do: assert to_number(5) == "V"
+  test "converts 10 to X", do: assert to_number(10) == "X"
 end
