cc7576a050e14edc9dbff9457d041d7be8780a8a
Cairo Noleto <caironoleto@gmail.com>
2017-02-28 14:11:43 -0300

    Adding test to 50, 100, 500 and 1000

--- day-004/lib/roman_numeral.ex
+++ day-004/lib/roman_numeral.ex
@@ -2,4 +2,8 @@ defmodule RomanNumeral do
   def to_number(1), do: "I"
   def to_number(5), do: "V"
   def to_number(10), do: "X"
+  def to_number(50), do: "L"
+  def to_number(100), do: "C"
+  def to_number(500), do: "D"
+  def to_number(1000), do: "M"
 end
--- day-004/test/roman_numeral_test.exs
+++ day-004/test/roman_numeral_test.exs
@@ -5,4 +5,8 @@ defmodule RomanNumeralTest do
   test "converts 1 to I", do: assert to_number(1) == "I"
   test "converts 5 to V", do: assert to_number(5) == "V"
   test "converts 10 to X", do: assert to_number(10) == "X"
+  test "converts 50 to X", do: assert to_number(50) == "L"
+  test "converts 100 to X", do: assert to_number(100) == "C"
+  test "converts 500 to X", do: assert to_number(500) == "D"
+  test "converts 1000 to X", do: assert to_number(1000) == "M"
 end
