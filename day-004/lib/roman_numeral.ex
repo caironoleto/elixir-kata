a69ab53ac2a8f400f4546018eda3b436bfb205cc
Cairo Noleto <caironoleto@gmail.com>
2017-02-28 14:17:09 -0300

    Using String.duplicate to return 2 and 3 values

--- day-004/lib/roman_numeral.ex
+++ day-004/lib/roman_numeral.ex
@@ -1,5 +1,5 @@
 defmodule RomanNumeral do
-  def to_number(1), do: "I"
+  def to_number(n) when n > 0 and n <= 3, do: String.duplicate("I", n)
   def to_number(5), do: "V"
   def to_number(10), do: "X"
   def to_number(50), do: "L"
--- day-004/test/roman_numeral_test.exs
+++ day-004/test/roman_numeral_test.exs
@@ -3,6 +3,8 @@ defmodule RomanNumeralTest do
   import RomanNumeral, only: [to_number: 1]
 
   test "converts 1 to I", do: assert to_number(1) == "I"
+  test "converts 2 to II", do: assert to_number(2) == "II"
+  test "converts 3 to III", do: assert to_number(3) == "III"
   test "converts 5 to V", do: assert to_number(5) == "V"
   test "converts 10 to X", do: assert to_number(10) == "X"
   test "converts 50 to X", do: assert to_number(50) == "L"
