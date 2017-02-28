759837111eba40d9f08754be3188821cbcb81966
Cairo Noleto <caironoleto@gmail.com>
2017-02-28 17:10:12 -0300

    Adding test for 4 :)

--- day-004/lib/roman_numeral.ex
+++ day-004/lib/roman_numeral.ex
@@ -1,5 +1,6 @@
 defmodule RomanNumeral do
   def to_number(n) when n > 0 and n <= 3, do: String.duplicate("I", n)
+  def to_number(4), do: "IV"
   def to_number(5), do: "V"
   def to_number(10), do: "X"
   def to_number(50), do: "L"
--- day-004/test/roman_numeral_test.exs
+++ day-004/test/roman_numeral_test.exs
@@ -5,6 +5,7 @@ defmodule RomanNumeralTest do
   test "converts 1 to I", do: assert to_number(1) == "I"
   test "converts 2 to II", do: assert to_number(2) == "II"
   test "converts 3 to III", do: assert to_number(3) == "III"
+  test "converts 4 to IV", do: assert to_number(4) == "IV"
   test "converts 5 to V", do: assert to_number(5) == "V"
   test "converts 10 to X", do: assert to_number(10) == "X"
   test "converts 50 to X", do: assert to_number(50) == "L"
